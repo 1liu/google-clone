2a5f5256699f2f7f39800e38fa7d0c179f22ed48
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-09-09 10:17:55 -0700

    installed mui dependencies and created Home and Search Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './pages/Home'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Google Clone</h1>
+    <div className="home">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
