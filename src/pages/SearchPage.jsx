aeb6d474530a912f1fd80741ad04d5fefe171f24
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-09-10 09:59:27 -0700

    completed searchPage options and styling

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -5,6 +5,11 @@ import './SearchPage.css'
 import useGoogleSearch from '../useGoogleSearch'
 import Search from '../components/Search'
 import SearchIcon from '@material-ui/icons/Search'
+import DescriptionIcon from '@material-ui/icons/Description'
+import LocalOfferIcon from '@material-ui/icons/LocalOffer'
+import ImageIcon from '@material-ui/icons/Image'
+import RoomIcon from '@material-ui/icons/Room'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import data from '../response'
 
 const SearchPage = (hideButton) => {
@@ -24,19 +29,49 @@ const SearchPage = (hideButton) => {
         </Link>
         <div className='searchPage_headerBody'>
           <Search hideButton />
+          <div className='searchPage_options'>
+            <div className="searchPage__options">
+              <div className="searchPage__optionLeft">
+                <div className="searchPage__option">
+                  <SearchIcon />
+                  <Link to='/all'>All</Link>
+                </div>
+                <div className="searchPage__option">
+                  <DescriptionIcon />
+                  <Link to='/news'>News</Link>
+                </div>
+                <div className="searchPage__option">
+                  <LocalOfferIcon />
+                  <Link to='/shopping'>Shopping</Link>
+                </div>
+                <div className="searchPage__option">
+                  <ImageIcon />
+                  <Link to='/images'>Images</Link>
+                </div>
+                <div className="searchPage__option">
+                  <RoomIcon />
+                  <Link to='/maps'>Maps</Link>
+                </div>
+                <div className="searchPage__option">
+                  <MoreVertIcon />
+                  <Link to='/more'>More</Link>
+                </div>
+              </div>
+              <div className="searchPage__optionRight">
+                <div className="searchPage__option">
+                  <Link to='/settings'>Settings</Link>
+                </div>
+                <div className="searchPage__option">
+                  <Link to='/tools'>Tools</Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='searchPage_options'>
-          {/* Resume here */}
-        </div>
+
       </div>
       <div className='searchPage__results'>
-        <SearchIcon />
-        <Link to='/all'>All</Link>
-        <Link to='/news'>News</Link>
-        <Link to='/shopping'>Shopping</Link>
-        <Link to='/images'>Images</Link>
-        <Link to='/maps'>Maps</Link>
-        <Link to='/more'>More</Link>
+
       </div>
     </div>
   )
